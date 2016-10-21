7a7640ce4f553ba6667a549e4c0832a3b3a79f17
Davide Bianchi <davide.bianchi@mondora.com>
2016-10-21 12:13:47 +0200

    Fixed log name

--- src/services/logger.js
+++ src/services/logger.js
@@ -2,7 +2,7 @@ import bunyan from "bunyan";
 
 import {LOG_LEVEL} from "../config";
 
-const logger = bunyan.createLogger({name: "fcm"});
+const logger = bunyan.createLogger({name: "notifications-aggregator"});
 
 logger.level(process.env.NODE_ENV === "test" ? "fatal" : LOG_LEVEL);
 
